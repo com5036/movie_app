105a191c44aa7482bc8b49b56fce9e00bb81f861
com5036 <qkrwl5036@naver.com>
2021-04-07 23:00:15 +0900

    update

--- src/App.js
+++ src/App.js
@@ -1,24 +1,57 @@
-import logo from './logo.svg';
-import './App.css';
+import PropTypes from "prop-types";
+
+
+function Food({name, picture, rating}){
+  return (
+    <div>
+      <h1>I like {name}</h1>
+      <img src={picture} alt="error"></img>
+      <h4>{rating}/5.0</h4>
+    </div>
+  );
+
+};
+
+Food.propTypes = {
+  name: PropTypes.string.isRequired,
+  picture : PropTypes.string.isRequired,
+  rating : PropTypes.number.isRequired
+}
+
+const foodILike = [
+  {
+    id :1,
+    name: "kimchi",
+    img : "https://cdn.imweb.me/thumbnail/20200415/6b6e035658bac.png",
+    rating : 5.0
+  },
+  {
+    id :2,
+    name : "chicken",
+    img : "https://rereco.co/wp-content/uploads/2018/02/best_menu09.jpg",
+    rating : 4.8
+  },
+  {
+    id :3,
+    name : "pizza",
+    img: "https://handmadepizza.co.kr/uploadFiles/TB_MENU_MAIN/img_pizza_%ED%8F%AC%ED%85%90%ED%94%BC%EC%9E%90.png",
+    rating : 4.8
+  }
+];
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+  <div>
+    {foodILike.map(dish => (
+      <Food 
+        key={dish.id}
+        name={dish.name} 
+        picture={dish.img} 
+        rating={dish.rating}
+       />
+    ))}
+  </div>
   );
 }
 
